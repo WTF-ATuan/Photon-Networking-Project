1f176b558d2946026a78ef829a7c11410fe04928
ATuan <4A8K0069@stust.edu.tw>
2021-10-08 10:24:13 +0800

    feat : CharacterMovement.cs and InputAdapter.cs

---
+++ Assets/Script/Main/Character/CharacterMovement.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+namespace Script.Main.Character{
+	public class CharacterMovement : MonoBehaviour{
+
+		[SerializeField] private float movementSpeed;
+		
+		private Rigidbody2D _rigidbody2D;
+		private SpriteRenderer _spriteRenderer;
+
+		public float Velocity{
+			get;
+			private set;
+		}
+
+		private void Start(){
+			_rigidbody2D = GetComponent<Rigidbody2D>();
+			_spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		private void Update(){
+			var horizontalValue = Input.GetAxisRaw($"Horizontal");
+			var verticalValue = Input.GetAxisRaw($"Vertical");
+			Move(horizontalValue, verticalValue);
+			SetFaceDirection(horizontalValue);
+		}
+
+		public void Move(float horizontal, float vertical){
+			var velocity = new Vector2(horizontal , vertical) * movementSpeed;
+			Velocity = velocity.magnitude;
+			_rigidbody2D.velocity = velocity;
+		}
+
+		public void SetFaceDirection(float facingValue){
+			var isRight = facingValue >= 0;
+			_spriteRenderer.flipX = !isRight;
+		}
+	}
+}
---
+++ Assets/Script/Main/InputData/InputAdapter.cs
@@ -0,0 +1,20 @@
+﻿using Script.Main.InputData.Event;
+using UnityEngine;
+
+namespace Script.Main.InputData{
+	public class InputAdapter : MonoBehaviour{
+		private void Update(){
+			DetectMoveInput();
+		}
+
+		private void DetectMoveInput(){
+			var horizontalValue = Input.GetAxisRaw($"Horizontal");
+			var verticalValue = Input.GetAxisRaw($"Vertical");
+			if(horizontalValue != 0 || verticalValue != 0){
+				EventBus.Post(new MoveInputDetected("123" , horizontalValue , verticalValue));
+			}
+
+		}
+		
+	}
+}
