488129b9b53fdb34b92b9287e86906a381caa410
ATuan <4A8K0069@stust.edu.tw>
2021-10-11 15:23:06 +0800

    Create OwnerID to let InputEvent catch

--- Assets/Script/Main/InputData/InputEventDetector.cs
+++ Assets/Script/Main/InputData/InputEventDetector.cs
@@ -3,6 +3,15 @@ using UnityEngine;
 
 namespace Script.Main.InputData{
 	public class InputEventDetector : MonoBehaviour{
+
+		public string OwnerID{
+			get;
+			private set;
+		}
+		public void Init(string ownerId){
+			OwnerID = ownerId;
+		}
+
 		private void Update(){
 			DetectMoveInput();
 			DetectBaseSkillInput();
@@ -12,17 +21,17 @@ namespace Script.Main.InputData{
 		private void DetectMoveInput(){
 			var horizontalValue = Input.GetAxisRaw($"Horizontal");
 			var verticalValue = Input.GetAxisRaw($"Vertical");
-			EventBus.Post(new MoveInputDetected("123" , horizontalValue , verticalValue));
+			EventBus.Post(new MoveInputDetected(OwnerID , horizontalValue , verticalValue));
 		}
 
 		private void DetectBaseSkillInput(){
 			if(Input.GetKeyDown(KeyCode.Q)){
-				EventBus.Post(new BaseSkillDetected("123"));
+				EventBus.Post(new BaseSkillDetected(OwnerID));
 			}
 		}
 		private void DetectStrongSkillInput(){
 			if(Input.GetKeyDown(KeyCode.E)){
-				EventBus.Post(new StrongSkillDetected("123"));
+				EventBus.Post(new StrongSkillDetected(OwnerID));
 			}
 		}
 
