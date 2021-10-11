f5ce147ec89ab87720db2e2dbac527171d37993a
江 <4a8k0046@stust.edu.tw>
2021-10-11 10:43:48 +0800

    生成FireBall
    按Q生成 FireBall可往前飛

---
+++ Assets/Script/Main/FierBall.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FierBall : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Initialize()
+    {
+        
+    }
+
+    void Update()
+    {
+        rigidbody.AddForce(transform.right*10);
+    }
+}
---
+++ Assets/Script/Main/UseSkill.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSkill : MonoBehaviour
+{
+    public GameObject FireBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            CreateSkill();
+        }
+    }
+
+    public void CreateSkill()
+    {
+        Instantiate(FireBall, gameObject.transform.position, Quaternion.identity);
+    }
+
+}
