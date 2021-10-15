a2182b24e9583c08e94e474274891b4d66e90575
ATuan <4A8K0069@stust.edu.tw>
2021-10-15 19:19:27 +0800

    create AbstractSkill.cs to base on Skill

---
+++ Assets/Script/Main/Character/Skill/AbstractSkill.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Script.Main.Character.Skill{
+	public abstract class AbstractSkill : MonoBehaviour{
+		public abstract string SkillName();
+		public abstract void InitSkill(SkillSpawnInfo spawnInfo);
+		public abstract float EnergyUsage();
+	}
+}
