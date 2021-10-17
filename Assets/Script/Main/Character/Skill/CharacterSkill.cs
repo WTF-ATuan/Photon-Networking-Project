8a0190789a4ec5b9ad252efd039511911b0c0a2d
ATuan <4A8K0069@stust.edu.tw>
2021-10-17 10:36:18 +0800

    refactor CharacterSkill.cs FindSkillCreatedTag

--- Assets/Script/Main/Character/Skill/CharacterSkill.cs
+++ Assets/Script/Main/Character/Skill/CharacterSkill.cs
@@ -7,18 +7,16 @@ namespace Script.Main.Character.Skill{
 		public List<SkillCreatedTag> skillList;
 
 		public void CastSkill(string skillName, SkillSpawnInfo data){
-			if(skillName == null){
-				throw new Exception("SkillName is Null");
-			}
-
-			var skill = skillList.Find(_ => _.skillName == skillName);
-			if(skill == null){
-				throw new Exception($"{skill} is not in List");
-			}
+			var skill = FindSkillCreatedTag(skillName);
 			skill.InitSkill(data);
 		}
 
 		public float GetSkillEnergyUsage(string skillName){
+			var skill = FindSkillCreatedTag(skillName);
+			return skill.energyUsage;
+		}
+
+		private SkillCreatedTag FindSkillCreatedTag(string skillName){
 			if(skillName == null){
 				throw new Exception("SkillName is Null");
 			}
@@ -28,7 +26,7 @@ namespace Script.Main.Character.Skill{
 				throw new Exception($"{skill} is not in List");
 			}
 
-			return skill.energyUsage;
+			return skill;
 		}
 	}
 }
