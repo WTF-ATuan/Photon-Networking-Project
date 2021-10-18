8883d8add43de45d7d7d98324bbc680b27b5717d
ATuan <4A8K0069@stust.edu.tw>
2021-10-18 21:29:21 +0800

    Create CollisionDetector.cs to detect skill collision

---
+++ Assets/Script/Main/Character/Skill/CollisionDetector.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Script.Main.Character.Skill{
+	public class CollisionDetector : MonoBehaviour{
+		public string OwnerID{ get; private set; }
+
+		public void InitDetector(string ownerID){
+			OwnerID = ownerID;
+		}
+
+		private void OnCollisionEnter(Collision other){ }
+
+		private void OnCollisionExit(Collision other){ }
+
+		private void OnCollisionEnter2D(Collision2D other){ }
+
+		private void OnCollisionExit2D(Collision2D other){ }
+	}
+}
