cb6d99a34f04b712380163573d03e5f001e2c261
ATuan <4A8K0069@stust.edu.tw>
2021-10-18 21:38:54 +0800

    新增 FireBall2D 新增2D 技能

---
+++ Assets/Script/Main/Character/Skill/SkillData/FireBall2D/FireBall2D.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Script.Main.Character.Skill.SkillData.FireBall2D{
+	public class FireBall2D : AbstractSkill{
+		public override void InitSkill(SkillSpawnInfo spawnInfo){
+			var direction = spawnInfo.Direction;
+			var rigidbody2D = GetComponent<Rigidbody2D>();
+			rigidbody2D.AddForce(direction * 50);
+		}
+	}
+}
