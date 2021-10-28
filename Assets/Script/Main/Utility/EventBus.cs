a1ace36d11062450d6d7b21bfdac0015a4092fd1
ATuan <4A8K0069@stust.edu.tw>
2021-10-28 11:40:32 +0800

    EventBus 可以有回傳值的事件了

--- Assets/Script/Main/Utility/EventBus.cs
+++ Assets/Script/Main/Utility/EventBus.cs
@@ -1,30 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Script.Main{
 	public class EventBus : MonoBehaviour{
-		private static readonly Dictionary<Type, List<Action<object>>> CallbackActions =
+		private static readonly Dictionary<Type, List<Action<object>>> NonCallbackActions =
 				new Dictionary<Type, List<Action<object>>>();
+
+		private static readonly Dictionary<Type, List<Func<object, object>>> CallbackActions =
+				new Dictionary<Type, List<Func<object, object>>>();
 
 		public static void Subscribe<T>(Action<T> callback){
 			var type = typeof(T);
-			var containsKey = CallbackActions.ContainsKey(type);
+			var containsKey = NonCallbackActions.ContainsKey(type);
 			if(containsKey){
-				var actions = CallbackActions[type];
+				var actions = NonCallbackActions[type];
 				actions.Add(o => callback((T)o));
 			}
 			else{
 				var actions = new List<Action<object>>{ o => callback((T)o) };
-				CallbackActions.Add(type, actions);
+				NonCallbackActions.Add(type, actions);
+			}
+		}
+
+		public static void Subscribe<T, TResult>(Func<T, TResult> callback){
+			var type = typeof(T);
+			var containsKey = CallbackActions.ContainsKey(type);
+			if(containsKey){
+				var callbackAction = CallbackActions[type];
+				callbackAction.Add(o => callback((T)o));
+			}
+			else{
+				var func = new List<Func<object, object>>{ o => callback((T)o) };
+				CallbackActions.Add(type, func);
 			}
 		}
 
 		public static void Post<T>(T obj){
 			var type = typeof(T);
-			var containsKey = CallbackActions.ContainsKey(type);
+			var containsKey = NonCallbackActions.ContainsKey(type);
 			if(containsKey){
-				var actions = CallbackActions[type];
+				var actions = NonCallbackActions[type];
 				actions.ForEach(o => o.Invoke(obj));
 			}
 			else{
@@ -32,10 +49,26 @@ namespace Script.Main{
 				Debug.Log($" Type {fullName}  is not Contain in EventBus");
 			}
 		}
+		public static TResult Post<T , TResult>(T obj){
+			var type = typeof(T);
+			var containsKey = CallbackActions.ContainsKey(type);
+			if(containsKey){
+				var callbackAction = CallbackActions[type];
+				foreach(var returnValue in callbackAction.Select(func => func.Invoke(obj))){
+					return (TResult) returnValue;
+				}
+			}
+			else{
+				var fullName = type.Name;
+				Debug.Log($" Type {fullName}  is not Contain in EventBus");
+			}
+			return default;
+		}
+		
 
 
 		private void OnDisable(){
-			CallbackActions.Clear();
+			NonCallbackActions.Clear();
 		}
 	}
 }
