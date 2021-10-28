2ac18de2a25fd60eef71c6955480ac56b84d19a4
ATuan <4A8K0069@stust.edu.tw>
2021-10-28 10:51:39 +0800

    把 character Skill 做的事情 改到 skill 當中

--- Assets/Script/Main/Skill/Skill.cs
+++ Assets/Script/Main/Skill/Skill.cs
@@ -1,9 +1,12 @@
-﻿using Script.Main.Character;
+﻿using System;
+using System.Collections.Generic;
+using Script.Main.Character;
 using Script.Main.Skill.SkillEvent;
 using UnityEngine;
 
 namespace Script.Main.Skill{
 	public class Skill : MonoBehaviour{
+		public List<SkillCreatedTag> skillList;
 		private void Start(){
 			EventBus.Subscribe<SkillCollide>(OnSkillCollide);
 		}
@@ -12,6 +15,18 @@ namespace Script.Main.Skill{
 			var collisionGameObject = obj.Collision.gameObject;
 			var enemy = collisionGameObject.GetComponent<IModifyHp>();
 			enemy?.ModifyHp(-10);
+		}
+		private SkillCreatedTag FindSkillCreatedTag(string skillName){
+			if(skillName == null){
+				throw new Exception("SkillName is Null");
+			}
+
+			var skill = skillList.Find(_ => _.name == skillName);
+			if(skill == null){
+				throw new Exception($"{skill} is not in List");
+			}
+
+			return skill;
 		}
 	}
 }
