c5925c408e7c41f5218be276d45758dc35cd1bcb
ATuan <4A8K0069@stust.edu.tw>
2021-10-29 23:36:47 +0800

    新增 CharacterEventHandler.cs 負責 Handler 所有 event

---
+++ Assets/Script/Main/Character/CharacterEventHandler.cs
@@ -0,0 +1,29 @@
+﻿using Script.Main.InputData.Event;
+
+namespace Script.Main.Character{
+	public class CharacterEventHandler{
+		private CharacterRepository CharacterRepository{ get; }
+
+		public CharacterEventHandler(){
+			CharacterRepository = new CharacterRepository();
+			EventBus.Subscribe<MoveInputDetected>(OnMoveInputDetected);
+			EventBus.Subscribe<BaseSkillDetected>(OnBaseSkillDetected);
+			EventBus.Subscribe<StrongSkillDetected>(OnStrongSkillDetected);
+		}
+
+		private void OnStrongSkillDetected(StrongSkillDetected obj){
+			var userId = obj.UserId;
+			var character = CharacterRepository.Query(userId);
+		}
+
+		private void OnBaseSkillDetected(BaseSkillDetected obj){
+			var userId = obj.UserId;
+			var character = CharacterRepository.Query(userId);
+		}
+
+		private void OnMoveInputDetected(MoveInputDetected obj){
+			var userId = obj.UserId;
+			var character = CharacterRepository.Query(userId);
+		}
+	}
+}
