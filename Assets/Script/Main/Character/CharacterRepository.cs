6a97e0d0d5412b3ca346936046430555f61f9fad
ATuan <4A8K0069@stust.edu.tw>
2021-10-29 23:33:13 +0800

    新增Repository 功能 Save 以及 Query

--- Assets/Script/Main/Character/CharacterRepository.cs
+++ Assets/Script/Main/Character/CharacterRepository.cs
@@ -1,5 +1,22 @@
-﻿namespace Script.Main.Character{
+﻿using System;
+using System.Collections.Generic;
+
+namespace Script.Main.Character{
 	public class CharacterRepository{
-		
+		private Dictionary<string, Character> _characters = new Dictionary<string, Character>();
+
+		public void Save(string characterID, Character character){
+			var containsKey = _characters.ContainsKey(characterID);
+			if(containsKey){
+				throw new Exception($"Character : {characterID} have same ID ");
+			}
+
+			_characters.Add(characterID, character);
+		}
+
+		public Character Query(string characterID){
+			var character = _characters[characterID];
+			return character ? character : default;
+		}
 	}
 }
