114067bdd3da298b8206be517ec7816bc83b2ad5
ATuan <4A8K0069@stust.edu.tw>
2021-10-30 16:39:54 +0800

    新增了一個singleton Object Repository 裡可以新增以及搜尋單一的物件

---
+++ Assets/Script/Main/Utility/SingleRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Script.Main.Utility{
+	public class SingleRepository : MonoBehaviour{
+		private static readonly Dictionary<Type, object> SingletonObjects = new Dictionary<Type, object>();
+
+		public static TReturn QueryObject<TReturn>() where TReturn : new(){
+			var type = typeof(TReturn);
+			var containsKey = SingletonObjects.ContainsKey(type);
+			if(containsKey){
+				var singletonObject = SingletonObjects[type];
+				return (TReturn)singletonObject;
+			}
+			else{
+				var singletonObject = new TReturn();
+				SingletonObjects.Add(type, singletonObject);
+				return singletonObject;
+			}
+		}
+
+	}
+}
---
+++ Assets/Script/Tests/SingleRepositoryTest.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Script.Main.Utility;
+
+namespace Script.Tests{
+	public class SingleRepositoryTest{
+		public int TestValue;
+
+		[Test]
+		public void Query_Should_Create_New_Instance(){
+			var current = SingleRepository.QueryObject<SingleRepositoryTest>();
+			Assert.NotNull(current);
+		}
+
+		[Test]
+		public void Repeatedly_Query_ShouldBe_Same_instance(){
+			var firstQuery = SingleRepository.QueryObject<SingleRepositoryTest>();
+			firstQuery.TestValue = 5;
+			var secondQuery = SingleRepository.QueryObject<SingleRepositoryTest>();
+			Assert.AreEqual(firstQuery.TestValue, secondQuery.TestValue);
+		}
+	}
+}
