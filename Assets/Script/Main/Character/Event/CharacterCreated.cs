fc497caef7be924ea47e9787df05196150ba60fb
ATuan <4A8K0069@stust.edu.tw>
2021-11-01 13:56:14 +0800

    新增 characterCreated 的事件 以及 characterSpawner 生成角色

---
+++ Assets/Script/Main/Character/Event/CharacterCreated.cs
@@ -0,0 +1,9 @@
+﻿namespace Script.Main.Character.Event{
+	public class CharacterCreated{
+		public string CharacterID{ get; }
+
+		public CharacterCreated(string characterID){
+			CharacterID = characterID;
+		}
+	}
+}
---
+++ Assets/Script/Main/CharacterSpawner.cs
@@ -0,0 +1,16 @@
+﻿using Script.Main.Character.Event;
+using UnityEngine;
+
+namespace Script.Main{
+	public class CharacterSpawner : MonoBehaviour{
+		[SerializeField] private GameObject characterPre;
+
+		private void Start(){
+			EventBus.Subscribe<CharacterCreated>(OnCharacterCreated);
+		}
+
+		private void OnCharacterCreated(CharacterCreated obj){
+						
+		}
+	}
+}
