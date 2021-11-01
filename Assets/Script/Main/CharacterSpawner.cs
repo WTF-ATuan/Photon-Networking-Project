462610aa7ce7d8a18c582b5e045b3f732a18c085
ATuan <4A8K0069@stust.edu.tw>
2021-11-01 15:42:21 +0800

    先硬做一個多人連線的版本 ，以及移除2D art 裡面 package 的腳本

--- Assets/Script/Main/CharacterSpawner.cs
+++ Assets/Script/Main/CharacterSpawner.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 namespace Script.Main{
 	public class CharacterSpawner : MonoBehaviour{
 		[SerializeField] private GameObject characterPre;
+		[SerializeField] private Sever sever;
 
 		private void Start(){
+			var randomPosition = Random.insideUnitCircle * 3;
+			var id = gameObject.GetInstanceID().ToString();
+			sever.GenerateItem(characterPre.name, randomPosition, Quaternion.identity, id);
 			EventBus.Subscribe<CharacterCreated>(OnCharacterCreated);
 		}
 
-		private void OnCharacterCreated(CharacterCreated obj){
-						
-		}
+		private void OnCharacterCreated(CharacterCreated obj){ }
 	}
 }
