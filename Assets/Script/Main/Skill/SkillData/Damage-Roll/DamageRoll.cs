0955e9064a2780ca8166ba4930569e01dfd948a8
ATuan <4A8K0069@stust.edu.tw>
2021-11-10 22:21:59 +0800

    新增腳本damageRoll 來發送翻滾事件

---
+++ Assets/Script/Main/Skill/SkillData/Damage-Roll/DamageRoll.cs
@@ -0,0 +1,12 @@
+﻿using Script.Main.Character.Event;
+
+namespace Script.Main.Skill.Damage_Roll{
+	public class DamageRoll : AbstractSkill{
+		public override void OnSkillCasted(SkillSpawnInfo spawnInfo){
+			var direction = spawnInfo.Direction;
+			var characterID = spawnInfo.OwnerID;
+			var originPosition = spawnInfo.OriginPosition;
+			EventBus.Post(new CharacterRolled(characterID, direction, 2));
+		}
+	}
+}
