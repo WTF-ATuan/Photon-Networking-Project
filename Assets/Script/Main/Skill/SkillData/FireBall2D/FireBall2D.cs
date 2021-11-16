02808f6a2e5d09d4e571ec7b084d91ff96593753
ATuan <4A8K0069@stust.edu.tw>
2021-11-17 00:27:48 +0800

    刪除全域變數改為inject 的模式

--- Assets/Script/Main/Skill/SkillData/FireBall2D/FireBall2D.cs
+++ Assets/Script/Main/Skill/SkillData/FireBall2D/FireBall2D.cs
@@ -8,37 +8,30 @@ namespace Script.Main.Skill.SkillData.FireBall2D{
 	public class FireBall2D : AbstractSkillData{
 		[SerializeField] private GameObject fireballViewObject;
 
-		private GameObject _fireballSkillObject;
-
-		private SkillSpawnInfo SpawnInfo{ get; set; }
-
 		public override void CastSkill(SkillSpawnInfo spawnInfo){
-			SpawnInfo = spawnInfo;
-			var originPosition = SpawnInfo.OriginPosition;
-			_fireballSkillObject = Instantiate(fireballViewObject, originPosition, Quaternion.identity);
-			_fireballSkillObject.OnCollisionEnter2DAsObservable()
-					.Subscribe(OnSkillObjectCollisionEnter)
-					.AddTo(_fireballSkillObject);
-			_fireballSkillObject.OnEnableAsObservable()
-					.Subscribe(x => OnSkillObjectCreated())
-					.AddTo(_fireballSkillObject);
+			var originPosition = spawnInfo.OriginPosition;
+			var skillObject = Instantiate(fireballViewObject, originPosition, Quaternion.identity);
+			skillObject.OnCollisionEnter2DAsObservable()
+					.Subscribe(x => OnSkillObjectCollisionEnter(skillObject, x))
+					.AddTo(skillObject);
+			OnSkillObjectCreated(skillObject, spawnInfo);
 		}
 
-		public void OnSkillObjectCreated(){
-			var direction = SpawnInfo.Direction;
-			var rigidbody2D = _fireballSkillObject.GetComponent<Rigidbody2D>();
+		private void OnSkillObjectCreated(GameObject skillObject, SkillSpawnInfo spawnInfo){
+			var direction = spawnInfo.Direction;
+			var rigidbody2D = skillObject.GetComponent<Rigidbody2D>();
 			rigidbody2D.AddForce(direction * 10, ForceMode2D.Impulse);
 		}
 
-		private void OnSkillObjectCollisionEnter(Collision2D other){
-			var rigidbody2D = _fireballSkillObject.GetComponent<Rigidbody2D>();
+		private void OnSkillObjectCollisionEnter(GameObject skillObject, Collision2D collision){
+			var rigidbody2D = skillObject.GetComponent<Rigidbody2D>();
 			rigidbody2D.Sleep();
-			DelayExplode(500);
+			DelayExplode(500, skillObject);
 		}
 
-		private async void DelayExplode(int delayTime){
+		private async void DelayExplode(int delayTime, GameObject skillObject){
 			await Task.Delay(delayTime);
-			var skillObjectPosition = _fireballSkillObject.transform.position;
+			var skillObjectPosition = skillObject.transform.position;
 			var colliders = Physics2D.OverlapCircleAll(skillObjectPosition, 2.5f);
 			foreach(var nearObjectCollider in colliders){
 				var rigidbodyComponent = nearObjectCollider.GetComponent<Rigidbody2D>();
@@ -49,7 +42,7 @@ namespace Script.Main.Skill.SkillData.FireBall2D{
 				rigidbodyComponent.AddForce(forceDirection * 3, ForceMode2D.Impulse);
 			}
 
-			Destroy(_fireballSkillObject);
+			Destroy(skillObject);
 		}
 	}
 }
