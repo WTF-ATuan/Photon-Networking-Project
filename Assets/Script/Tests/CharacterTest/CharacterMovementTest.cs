5437044efdb0cac67d6bea219bf02306d5b27e34
ATuan <4A8K0069@stust.edu.tw>
2021-11-17 00:59:23 +0800

    測試撰寫 已完成~

--- Assets/Script/Tests/CharacterTest/CharacterMovementTest.cs
+++ Assets/Script/Tests/CharacterTest/CharacterMovementTest.cs
@@ -7,12 +7,28 @@ using UnityEngine.TestTools;
 
 namespace Script.Tests.CharacterTest{
 	public class CharacterMovementTest{
-
 		[Test]
 		public void MoveVelocity(){
 			var gameObject = new GameObject();
 			var movement = gameObject.AddComponent<CharacterMovement>();
+			var horizontal = 1;
+			var vertical = 1;
+			var speed = 10;
+			var expect = new Vector2(horizontal, vertical) * speed;
+			var current = movement.GetMoveVelocity(horizontal, vertical, speed);
+			Assert.AreEqual(expect, current);
 		}
 
+		[Test]
+		public void RollMove(){
+			var gameObject = new GameObject();
+			var movement = gameObject.AddComponent<CharacterMovement>();
+			var originPosition = new Vector2(1, 1);
+			var direction = new Vector2(1, 1);
+			var force = 10;
+			var expect = originPosition + (direction * 10);
+			var current = movement.GetRollTargetPosition(originPosition, direction, force);
+			Assert.AreEqual(expect, current);
+		}
 	}
 }
