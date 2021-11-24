66c26cfe7b5bccab8f5f78c3d115e6e71deaa7ac
ATuan <4A8K0069@stust.edu.tw>
2021-11-24 16:45:38 +0800

    更改Enemy 的檔案位置

---
+++ Assets/Script/Main/Enemy/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI ;
+
+namespace Script.Main.Character{
+	public class Enemy : MonoBehaviour , IModifyHp{
+		public float hp = 100;
+        public Image HPbar;
+
+		public void ModifyHp(float amount){
+			hp += amount;
+			HPbar.GetComponent<RectTransform>().sizeDelta= new Vector2(hp -amount, 10);
+			if(hp <= 0){
+				gameObject.SetActive(false);
+			}
+		}
+	}
+
+	public interface IModifyHp{
+		void ModifyHp(float amount);
+
+
+	}
+
+    
+}
