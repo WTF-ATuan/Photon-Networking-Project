ccd3d307bdbaf54f78b0b8b04e2b385473f3acd9
江 <4a8k0046@stust.edu.tw>
2021-11-26 01:23:34 +0800

    相機跟隨(用tag *Player* 抓取角色 *相機的跟隨目標* )

---
+++ Assets/Script/Jiang/CameraFollow.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Player;
+    public float smothing = 5f;   //鏡頭平滑移動的速度
+    Vector3 offset;  //相機和主角之間的固定距離
+
+    void Start()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player").transform;
+        offset = transform.position - Player.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCampos = Player.position + offset;
+
+        transform.position = Vector3.Lerp(transform.position, targetCampos, smothing * Time.deltaTime);  //以一定的速度，從A平滑移動到B
+    }
+}
+
