f19619d8acee44f55eef570e3cf008631037aaff
ATuan <4A8K0069@stust.edu.tw>
2021-11-25 18:02:22 +0800

    做了兩個API 獲得 最近玩家的位置 以及 敵人與玩家之間的距離 可以用事件去驅動他

--- Assets/Script/Main/Enemy/EnemyBehavior.cs
+++ Assets/Script/Main/Enemy/EnemyBehavior.cs
@@ -1,14 +1,43 @@
 ﻿using Script.Main.Character;
 using Script.Main.Utility;
+using UnityEngine;
 
 namespace Script.Main.Enemy{
 	public class EnemyBehavior{
-		private EnemyRepository _enemyRepository;
 		private CharacterRepository _characterRepository;
+		private EnemyRepository _enemyRepository;
 
 		public EnemyBehavior(){
 			_enemyRepository = SingleRepository.Query<EnemyRepository>();
 			_characterRepository = SingleRepository.Query<CharacterRepository>();
+		}
+
+		public Vector3 GetClosestCharacterPosition(string enemyID){
+			var enemy = _enemyRepository.Query(enemyID);
+			var enemyPosition = enemy.transform.position;
+			var characters = _characterRepository.QueryAll();
+			var minDistance = float.PositiveInfinity;
+			Character.Character closestCharacter = null;
+			foreach(var character in characters){
+				var characterPosition = character.transform.position;
+				var distance = Vector3.Distance(characterPosition, enemyPosition);
+				if(!(distance < minDistance)) continue;
+				closestCharacter = character;
+				minDistance = distance;
+			}
+
+			var position = closestCharacter ? closestCharacter.transform.position : Vector3.zero;
+
+			return position;
+		}
+
+		public float GetDistanceOfCharacter(string enemyID, string characterID){
+			var enemy = _enemyRepository.Query(enemyID);
+			var character = _characterRepository.Query(characterID);
+			var enemyPosition = enemy.transform.position;
+			var characterPosition = character.transform.position;
+			var distance = Vector3.Distance(characterPosition, enemyPosition);
+			return distance;
 		}
 	}
 }
