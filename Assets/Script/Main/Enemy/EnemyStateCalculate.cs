99511b98a3eab3ea1332320c8854241a8e50f322
ATuan <4A8K0069@stust.edu.tw>
2021-11-25 18:07:29 +0800

    新增 construster 讓測試運行

--- Assets/Script/Main/Enemy/EnemyStateCalculate.cs
+++ Assets/Script/Main/Enemy/EnemyStateCalculate.cs
@@ -12,6 +12,11 @@ namespace Script.Main.Enemy{
 			_characterRepository = SingleRepository.Query<CharacterRepository>();
 		}
 
+		public EnemyStateCalculate(CharacterRepository characterRepository, EnemyRepository enemyRepository){
+			_characterRepository = characterRepository;
+			_enemyRepository = enemyRepository;
+		}
+
 		public Vector3 GetClosestCharacterPosition(string enemyID){
 			var enemy = _enemyRepository.Query(enemyID);
 			var enemyPosition = enemy.transform.position;
