cf24e21ccd72e559f4e03226504600f3f4aec41f
ATuan <4A8K0069@stust.edu.tw>
2021-11-27 16:23:10 +0800

    refactor Enemybehavior

---
+++ Assets/Script/Main/Enemy/EnemyBehavior.cs
@@ -0,0 +1,27 @@
+﻿using Script.Main.Utility;
+using UnityEngine;
+
+namespace Script.Main.Enemy{
+	public class EnemyBehavior{
+		private Enemy Enemy{ get; }
+
+		private readonly EnemyStateCalculated _stateCalculated;
+
+		public EnemyBehavior(Enemy enemy){
+			Enemy = enemy;
+			_stateCalculated = SingleRepository.Query<EnemyStateCalculated>();
+		}
+
+		public void UpdateState(){
+			var closestCharacterDistance = _stateCalculated.GetClosestCharacterDistance(Enemy.ID);
+			var characterPosition = _stateCalculated.GetClosestCharacterPosition(Enemy.ID);
+			if(closestCharacterDistance <= 3){
+				Enemy.Attack(characterPosition);
+				return;
+			}
+
+			var movePosition = (Vector2)characterPosition + Random.insideUnitCircle * 3;
+			Enemy.Move(movePosition);
+		}
+	}
+}
