f9017d3604ab4584590febb60dd1b9eac8db6ff4
江 <4a8k0046@stust.edu.tw>
2021-11-30 14:55:12 +0800

    寫(ScriptableObject)Avatar腳本，可從Creat看到Avatar項目直接新建

---
+++ Assets/Script/Jiang/Avatars.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Avatar", fileName = "NewAvatar")]
+public class Avatars : ScriptableObject
+{
+    public Sprite AvatarImage;
+    public Sprite AvatarIntroduce;
+    public Sprite SkillIRollSkill;
+    public Sprite BaseSkill;
+    public Sprite StrongSkill;
+}
