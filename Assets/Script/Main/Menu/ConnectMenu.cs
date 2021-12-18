cab4ce79a328cbcfd2a510f24dd8f0b3f2629205
ATuan <4A8K0069@stust.edu.tw>
2021-12-18 15:41:26 +0800

    Menu Setting

--- Assets/Script/Main/Menu/ConnectMenu.cs
+++ Assets/Script/Main/Menu/ConnectMenu.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿using Script.Main.Server.Event;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace Script.Main.Menu{
 	public class ConnectMenu : MonoBehaviour{
-
 		[SerializeField] private InputField sessionID;
+		[SerializeField] private SceneObject scene;
+
 
 		public void ConnectSever(){
-			
+			EventBus.Post(new SeverConnected(true, sessionID.text, scene.ToString()));
 		}
 
 		public void ConnectClient(){
-			
+			EventBus.Post(new SeverConnected(false, sessionID.text, scene.ToString()));
 		}
 	}
 }
