285d067074003e6d6b8762f176bb8399d2b499fd
ATuan <4A8K0069@stust.edu.tw>
2021-12-19 13:28:20 +0800

    view Event 接收器 接收角色移動事件

---
+++ Assets/Script/Main/Character/Component/CharacterViewEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using Photon.Bolt;
+using Script.Main.Character.Event;
+using UnityEngine;
+
+namespace Script.Main.Character.Component{
+	public class CharacterViewEventHandler : EntityBehaviour<ICharacterState>{
+		public override void Attached(){
+			EventBus.Subscribe<CharacterMoved>(OnCharacterMoved);
+			state.SetTransforms(state.CharacterTranform, transform);
+		}
+
+		private void OnCharacterMoved(CharacterMoved obj){
+			var originPosition = obj.OriginPosition;
+			var targetPosition = obj.TargetPosition;
+			
+		}
+	}
+}
