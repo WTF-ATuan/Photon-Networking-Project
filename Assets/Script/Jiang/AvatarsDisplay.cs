d11fea0d29a50e25b381c686f4148fd4db79c70d
江 <4a8k0046@stust.edu.tw>
2021-12-19 12:05:52 +0800

    建立 顯示人物、人物介紹、技能、技能介紹......的腳本，掛載到選角畫面的Canvas

---
+++ Assets/Script/Jiang/AvatarsDisplay.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AvatarsDisplay : MonoBehaviour
+{
+    public Image AvatarImage;
+    public Image AvatarIntroduce;
+    public Image SkillIRollSkill;
+    public Image BaseSkill;
+    public Image StrongSkill;
+    public Image SkillIntroduce;
+
+    public static  string ChickWhichSkillntroduce;
+
+    public Avatars[] avatars = new Avatars[4];
+    Avatars avatar;
+
+
+    void Start()
+    {
+        AvatarImage.sprite = avatar.AvatarImage;
+        AvatarIntroduce.sprite = avatar.AvatarIntroduce;
+        SkillIRollSkill.sprite = avatar.SkillIRollSkill;
+        BaseSkill.sprite = avatar.BaseSkill;
+        StrongSkill.sprite = avatar.StrongSkill;
+    }
+
+    void FixedUpdate()
+    {
+        if (ChickWhichSkillntroduce == "SkillIRollSkill")
+        {
+            SkillIntroduce.sprite = avatar.IntroductionOfSkillIRollSkill;
+        }
+        else if (ChickWhichSkillntroduce == "BaseSkill")
+        {
+            SkillIntroduce.sprite = avatar.IntroductionOfBaseSkill;
+        }
+        else if (ChickWhichSkillntroduce == "StrongSkill")
+        {
+            SkillIntroduce.sprite = avatar.IntroductionOfStrongSkill;
+        }
+    }
+}
