9fe8d35d828d5e35e67a16efaac4d0ef88827ad3
ATuan <4A8K0069@stust.edu.tw>
2021-12-21 22:14:48 +0800

    技能修正

--- Assets/Script/Main/Character/Attack/SpeedKnife.cs
+++ Assets/Script/Main/Character/Attack/SpeedKnife.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using Photon.Bolt;
-using Script.Main.Character.Interface;
+using Script.Main.Enemy.Interface;
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 using UnityEngine.Serialization;
+using IAttack = Script.Main.Character.Interface.IAttack;
 
 namespace Script.Main.Character.Attack{
 	public class SpeedKnife : MonoBehaviour, IAttack{
@@ -31,25 +34,34 @@ namespace Script.Main.Character.Attack{
 				StopCoroutine(_throwingTimer);
 			}
 
-			_throwingTimer = StartCoroutine(SecondThrowTimer(attackDirection, targetPosition));
+			_throwingTimer = StartCoroutine(SecondThrowTimer(attackDirection));
 		}
 
-		private IEnumerator SecondThrowTimer(Vector2 direction, Vector2 targetPosition){
-			var entity = Instantiate(knifePre, transform.position + transform.right * 0.3f, Quaternion.identity);
-			entity.transform.LookAt(targetPosition);
-			var skillRig = entity.GetComponent<Rigidbody2D>();
-			ThrowKnife(skillRig, direction);
+		private IEnumerator SecondThrowTimer(Vector2 direction){
+			var entity = Instantiate(knifePre, transform.position + new Vector3(transform.right.x, 1, 0),
+				Quaternion.identity);
+			entity.transform.right = direction;
+			ThrowKnife(entity, direction);
 			yield return new WaitForSeconds(0.5f);
-			var secondEntity = Instantiate(knifePre, transform.position + transform.right * 0.3f, Quaternion.identity);
-			secondEntity.transform.LookAt(targetPosition);
-			var secondSkillRig = secondEntity.GetComponent<Rigidbody2D>();
-			ThrowKnife(secondSkillRig, direction);
+			var secondEntity = Instantiate(knifePre, transform.position + new Vector3(transform.right.x, 1, 0),
+				Quaternion.identity);
+			secondEntity.transform.right = direction;
+			ThrowKnife(secondEntity, direction);
 		}
 
-		private void ThrowKnife(Rigidbody2D knifeRig, Vector2 direction){
+		private void ThrowKnife(GameObject knife, Vector2 direction){
+			knife.OnTriggerEnter2DAsObservable()
+					.Subscribe(x => OnSkillTriggered(knife, x))
+					.AddTo(knife);
+			var knifeRig = knife.GetComponent<Rigidbody2D>();
 			knifeRig.AddForce(direction * throwPower, ForceMode2D.Impulse);
+			Destroy(knife, 5f);
 		}
 
-		private void OnSkillTriggered(BoltEntity skillObjectClone, Collision2D other){ }
+		private void OnSkillTriggered(GameObject skillObjectClone, Collider2D other){
+			var modifyHp = other.GetComponent<IModifyHp>();
+			modifyHp?.ModifyHp(-5);
+			Destroy(skillObjectClone);
+		}
 	}
 }
