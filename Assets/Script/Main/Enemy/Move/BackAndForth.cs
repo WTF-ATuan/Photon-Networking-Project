926c8416134eef981f0f6a848590735128425340
ATuan <4A8K0069@stust.edu.tw>
2021-12-25 19:53:40 +0800

    折返跑 怪物移動

---
+++ Assets/Script/Main/Enemy/Move/BackAndForth.cs
@@ -0,0 +1,30 @@
+﻿using Script.Main.Enemy.Interface;
+using UnityEngine;
+
+namespace Script.Main.Enemy.Move{
+	public class BackAndForth : MonoBehaviour, IMove{
+		[SerializeField] private bool defaultFaceDirectionIsRight;
+
+
+		private Enemy _enemy;
+		private bool _faceDirectionIsRight;
+
+		private void Start(){
+			_enemy = GetComponent<Enemy>();
+			_faceDirectionIsRight = defaultFaceDirectionIsRight;
+			_enemy.SetFacingDirection(_faceDirectionIsRight);
+		}
+
+		public void Move(bool enable){
+			DetectFacing();
+			transform.position += _enemy.GetFacingDirection() * Time.deltaTime;
+		}
+
+		private void DetectFacing(){
+			var targetList = _enemy.Detect<Collider2D>();
+			var isEmpty = targetList.Count < 1;
+			if(isEmpty) return;
+			_enemy.SetFacingDirection(_faceDirectionIsRight);
+		}
+	}
+}
