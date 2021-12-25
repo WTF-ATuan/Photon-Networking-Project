46f801f595af476a5a2afb43584f02bd3b9337f6
ATuan <4A8K0069@stust.edu.tw>
2021-12-25 18:53:17 +0800

    forward script added

---
+++ Assets/Script/Main/Enemy/Move/Forward.cs
@@ -0,0 +1,16 @@
+﻿using Script.Main.Enemy.Interface;
+using UnityEngine;
+
+namespace Script.Main.Enemy.Move{
+	public class Forward : MonoBehaviour , IMove{
+		
+		
+		public void Move(){
+			DetectFacing();
+		}
+		private void DetectFacing(){
+			var positionX = transform.position.x;
+			
+		}
+	}
+}
