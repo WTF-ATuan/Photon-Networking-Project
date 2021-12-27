e29c9d35de0481432d59796250c0d7f4338d0e67
ATuan <4A8K0069@stust.edu.tw>
2021-12-27 15:38:37 +0800

    ColdDownTimer and CloseAttack

---
+++ Assets/Script/Main/Enemy/Extension/ColdDownTimer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Script.Main.Enemy.Extension{
+	public class ColdDownTimer{
+		public float During{ get; private set; }
+		private float TrackTime{ get; set; }
+
+		public ColdDownTimer(float during){
+			During = during;
+			TrackTime = Time.time;
+		}
+
+		public void ModifyDuring(float during){
+			if(during <= 0) return;
+			During = during;
+		}
+
+		public bool CanInvoke(){
+			var currentTime = Time.time;
+			return currentTime > TrackTime;
+		}
+
+		public void ResetColdTimer(){
+			var currentTime = Time.time;
+			TrackTime = currentTime + During;
+		}
+	}
+}
