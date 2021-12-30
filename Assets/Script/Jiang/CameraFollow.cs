a66b210ff421b5d968a2097e4c54551d5049e2f4
ATuan <4A8K0069@stust.edu.tw>
2021-12-30 20:28:23 +0800

    相機最大距離設置

--- Assets/Script/Jiang/CameraFollow.cs
+++ Assets/Script/Jiang/CameraFollow.cs
@@ -1,5 +1,7 @@
 using Script.Main;
 using Script.Main.Character.Event;
+using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -9,8 +11,22 @@ namespace Script.Jiang{
 		public float smoothing = 5f; //鏡頭平滑移動的速度
 		private Vector3 _offset; //相機和主角之間的固定距離
 
+		[SerializeField] [LabelText("相機最大移動距離")] [PropertyOrder(0)]
+		private float maxMoveLimitX = 10f;
+
+		private float leftLimitX;
+		private float rightLimitX;
+
+
 		private void Start(){
 			EventBus.Subscribe<CharacterCreated>(OnCharacterCreated);
+			ProcessPatrolPositions();
+		}
+
+		private void ProcessPatrolPositions(){
+			var spawnPositionX = transform.position.x;
+			leftLimitX = spawnPositionX - maxMoveLimitX;
+			rightLimitX = spawnPositionX + maxMoveLimitX;
 		}
 
 		private void OnCharacterCreated(CharacterCreated obj){
@@ -20,9 +36,29 @@ namespace Script.Jiang{
 		}
 
 		private void FixedUpdate(){
-			var targetCampos = player.position + _offset + Vector3.up;
+			var targetPosition = player.position + _offset + Vector3.up;
+			var targetPositionX = targetPosition.x;
+			if(targetPositionX > rightLimitX || targetPositionX < leftLimitX) return;
 			transform.position =
-					Vector3.Lerp(transform.position, targetCampos, smoothing * Time.deltaTime); //以一定的速度，從A平滑移動到B
+					Vector3.Lerp(transform.position, targetPosition, smoothing * Time.deltaTime); //以一定的速度，從A平滑移動到B
+		}
+
+		private void DrawLine(float patrolX, Vector3 spawnPosition){
+			var spawnPositionY = spawnPosition.y;
+			var spawnPositionZ = spawnPosition.z;
+			var startPoint = new Vector3(patrolX, spawnPositionY + 2f, spawnPositionZ);
+			var endPoint = new Vector3(patrolX, spawnPositionY - 1f, spawnPositionZ);
+			Gizmos.DrawLine(startPoint, endPoint);
+		}
+
+
+		private void OnDrawGizmos(){
+			Gizmos.color = Color.white;
+			if(Application.isPlaying == false)
+				ProcessPatrolPositions();
+			var position = transform.position;
+			DrawLine(leftLimitX, position);
+			DrawLine(rightLimitX, position);
 		}
 	}
 }
