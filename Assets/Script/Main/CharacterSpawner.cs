ac801fb34c07a6e2675e4f5f43b056890fb2bd3b
ATuan <4A8K0069@stust.edu.tw>
2022-01-01 18:08:45 +0800

    封包 方法 拆開 server 以及 local

--- Assets/Script/Main/CharacterSpawner.cs
+++ Assets/Script/Main/CharacterSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using Photon.Bolt;
 using Script.Main.Character.Event;
 using Script.Main.InputData;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace Script.Main{
@@ -12,19 +13,27 @@ namespace Script.Main{
 			var randomPosition = Random.insideUnitCircle * 3;
 			var isRunning = BoltNetwork.IsRunning;
 			if(isRunning){
-				var entity = BoltNetwork.Instantiate(characterPre, randomPosition, Quaternion.identity);
-				var character = entity.GetComponent<Character.Character>();
-				var id = entity.NetworkId.ToString();
-				character.gameObject.AddComponent<InputEventDetector>().Init(id);
-				EventBus.Post(new CharacterCreated(id, character));
+				CreateCharacterOnServer(randomPosition);
 			}
 			else{
-				var entity = Instantiate(characterPre, randomPosition, Quaternion.identity);
-				var character = entity.GetComponent<Character.Character>();
-				var id = entity.GetInstanceID().ToString();
-				character.gameObject.AddComponent<InputEventDetector>().Init(id);
-				EventBus.Post(new CharacterCreated(id, character));
+				CreateCharacterOnLocal(randomPosition);
 			}
+		}
+
+		public void CreateCharacterOnServer(Vector3 spawnPosition){
+			var entity = BoltNetwork.Instantiate(characterPre, spawnPosition, Quaternion.identity);
+			var character = entity.GetComponent<Character.Character>();
+			var id = entity.NetworkId.ToString();
+			character.gameObject.AddComponent<InputEventDetector>().Init(id);
+			EventBus.Post(new CharacterCreated(id, character));
+		}
+		[Button]
+		public void CreateCharacterOnLocal(Vector3 spawnPosition){
+			var entity = Instantiate(characterPre, spawnPosition, Quaternion.identity);
+			var character = entity.GetComponent<Character.Character>();
+			var id = entity.GetInstanceID().ToString();
+			character.gameObject.AddComponent<InputEventDetector>().Init(id);
+			EventBus.Post(new CharacterCreated(id, character));
 		}
 	}
 }
